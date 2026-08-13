dd9df993e240e1cdca4de6cfff7fc13c3a5ab20d
ShujaaQureshii <jkmerowww@gmail.com>
2026-08-13 12:21:45 +0500

    Update WasmBuilder.js path in test file

--- wbtests/semantics/result_mismatch/semantics_result_mismatch.js
+++ wbtests/semantics/result_mismatch/semantics_result_mismatch.js
@@ -1,4 +1,4 @@
-load("test/mjsunit/wasm/WasmBuilder.js");
+load("wbunit/wasm/WasmBuilder.js");
 
 const mb = new WasmModuleBuilder();
 const f = mb.addFunction("r", { params: [], results: ['i64'] });
