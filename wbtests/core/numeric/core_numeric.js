16a449929b6c0fca3b00aadef881a184b7984b34
ShujaaQureshii <jkmerowww@gmail.com>
2026-08-13 11:52:35 +0500

    Update import path for WasmBuilder.js

--- wbtests/core/numeric/core_numeric.js
+++ wbtests/core/numeric/core_numeric.js
@@ -1,4 +1,4 @@
-load("test/mjsunit/wasm/WasmBuilder.js");
+load("wbunit/wasm/WasmBuilder.js");
 
 const mb = new WasmModuleBuilder();
 const f = mb.addFunction("pick", { params: ['i32', 'i32'], results: ['i32'] });
