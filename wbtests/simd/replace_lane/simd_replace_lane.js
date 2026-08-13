05ca0606d4c0f087f135ceb0b5b93710c00727b9
ShujaaQureshii <jkmerowww@gmail.com>
2026-08-13 12:29:08 +0500

    Update simd_replace_lane.js

--- wbtests/simd/replace_lane/simd_replace_lane.js
+++ wbtests/simd/replace_lane/simd_replace_lane.js
@@ -1,4 +1,4 @@
-load("test/mjsunit/wasm/WasmBuilder.js");
+load("wbunit/wasm/WasmBuilder.js");
 
 const mb = new WasmModuleBuilder();
 const f = mb.addFunction("replace_one", { params: [], results: ['i32'] });
