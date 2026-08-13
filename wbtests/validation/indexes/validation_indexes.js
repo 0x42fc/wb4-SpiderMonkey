f8353f923a82a368d09d097fe0270a6da2fe39e5
ShujaaQureshii <jkmerowww@gmail.com>
2026-08-13 12:33:15 +0500

    Update validation_indexes.js

--- wbtests/validation/indexes/validation_indexes.js
+++ wbtests/validation/indexes/validation_indexes.js
@@ -1,4 +1,4 @@
-load("test/mjsunit/wasm/WasmBuilder.js");
+load("wbunit/wasm/WasmBuilder.js");
 
 const mb = new WasmModuleBuilder();
 const f = mb.addFunction("bad_local", { params: [], results: ['i32'] });
