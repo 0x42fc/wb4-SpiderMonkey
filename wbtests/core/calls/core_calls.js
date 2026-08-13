359d6820221f7c7d40d98b2ef573cc3c8079ee26
ShujaaQureshii <jkmerowww@gmail.com>
2026-08-13 11:31:42 +0500

    Added comments for clarity

--- wbtests/core/calls/core_calls.js
+++ wbtests/core/calls/core_calls.js
@@ -1,6 +1,9 @@
 load("test/mjsunit/wasm/WasmBuilder.js");
 
+// imported in JS shell or env.
 const mb = new WasmModuleBuilder();
+
+// helper
 const helper = mb.addFunction("double", { params: ['i32'], results: ['i32'] });
 helper.body([
   ['local.get', 0],
@@ -9,14 +12,24 @@ helper.body([
   ['end']
 ]);
 
-const main = mb.addFunction("quad", { params: ['i32'], results: ['i32'] });
+// main
+// quad, single param..
+const main = mb.addFunction("quad", { 
+  params: ['i32'], 
+  results: ['i32'] });
+
+// body
 main.body([
   ['local.get', 0],
   ['call', 'double'],
   ['call', 'double'],
   ['end']
 ]);
+
+// exported 
 main.exportAs("quad");
 
-const instance = mb.instantiate({});
-const result = instance.exports.quad(5);
+const instance = mb.instantiate({}); // instantiation
+const result = instance.exports.quad(5); // exported via JS
+// console.log(instance, "Expected!");
+print(instance, "Expected!");
