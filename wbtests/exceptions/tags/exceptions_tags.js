ba6f9212cad5ed7323d7721a75d5ab6b1c63c6ce
ShujaaQureshii <jkmerowww@gmail.com>
2026-08-14 09:35:51 -0700

    Opps, we have changed something, will revert it as soon as possible.

--- wbtests/exceptions/tags/exceptions_tags.js
+++
@@ -1,8 +0,0 @@
-load("wbunit/wasm/WasmBuilder.js");
-
-const mb = new WasmModuleBuilder();
-const tag = mb.addTag({ params: ['i32'], results: [] });
-mb.exportTag(tag, "t");
-
-const instance = mb.instantiate({});
-const exported = instance.exports.t;
--- wbtests/exports/tables/exports_tables.js
+++
@@ -1,7 +0,0 @@
-load("wbunit/wasm/WasmBuilder.js");
-
-const mb = new WasmModuleBuilder();
-mb.addTable('funcref', 2);
-mb.exportTable(0, "tbl");
-
-const instance = mb.instantiate({});
